9cb1a7347ef95464862a2347b291bc74016ddc14
Alexandr Zlobin <sasha_zlobni@ukr.net>
2021-08-24 13:55:26 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  
+    $('.discount').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+      // autoplaySpeed:2000
+    })
+
+    let mixer1 = mixitup('.product__card');
+    let mixer2 = mixitup('.sale__card');
+
+
+   
+  })
